e09cb827244f9855377f9d226ebc73d8d492ac46
salomao91 <salomao1991@gmail.com>
2025-04-03 16:42:45 -0400

    data binding with methodes

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -3,8 +3,19 @@ const app = Vue.createApp({
         return {
             courseGoal: 'Finish the course and learn Vue!',
             vueLink: 'https://vuejs.org'
+        };
+    },
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                return 'Learning Vue!';
+            } else {
+                return 'Mastering Vue!';
+            }
         }
     }
+
 });
 
 app.mount('#user-goal');
