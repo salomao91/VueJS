2efe72643a1778dc391e91f522eb3f5d5caf2329
salomao91 <salomao1991@gmail.com>
2025-04-04 16:02:41 -0400

    assignment 2 solution

--- basics-assignment-2-problem/app.js
+++ basics-assignment-2-problem/app.js
@@ -1,10 +1,19 @@
 Vue.createApp({
     data() {
         return {
-
+            userInputValue: '',
+            userInputValueWhenEnter: ''
         };
     },
     methods: {
-
+        btnShowAlert() {
+            alert('Boutton clicked!');
+        },
+        btnSaveInput(event) {
+            this.userInputValue = event.target.value;
+        },
+        btnSaveInputWhenEnter(event) {
+            this.userInputValueWhenEnter = event.target.value;
+        }
     }
 }).mount('#assignment');
