12a30c941d427aae7cdd203b786839a7b98363d6
salomao91 <salomao1991@gmail.com>
2025-04-05 16:34:46 -0400

    assignment 3 solution - computed and watch

--- basics-assignment-3-problem/app.js
+++ basics-assignment-3-problem/app.js
@@ -1,10 +1,32 @@
 Vue.createApp({
     data() {
         return {
-            
+            counter: 0
+        }
+    },
+    computed: {
+        message() {
+            if (this.counter < 37) {
+                return 'Not there yet';
+            } else if (this.counter === 37) {
+                return this.counter;
+            } else {
+                return 'Too much!';
+            }
+        }
+    },
+    watch: {
+        //it watches the computed property 'message', when it changes this watch it executed/triggered
+        message() {
+            console.log('watcher executing...');
+            setTimeout(() => {
+                this.counter = 0;
+            }, 5000);
         }
     },
     methods: {
-        
+        btnIncrementCounter(number) {
+            this.counter = this.counter + number;
+        }
     }
 }).mount('#assignment');
