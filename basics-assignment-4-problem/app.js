0ce557bbbf6a89858598049a3561f4955437cb8c
salomao91 <salomao1991@gmail.com>
2025-04-07 13:24:51 -0400

    assignment 4 partial solution - move array of classes from html to vue code and returning it using computed

--- basics-assignment-4-problem/app.js
+++ basics-assignment-4-problem/app.js
@@ -9,5 +9,15 @@ Vue.createApp({
         btnToggle() {
             this.isParagraphVisible = !this.isParagraphVisible;
         }
+    },
+    computed: {
+        classesList() {
+            return {
+                user1: this.userInput === 'user1',
+                user2: this.userInput === 'user2',
+                visible: this.isParagraphVisible,
+                hidden: !this.isParagraphVisible
+            };
+        }
     }
 }).mount('#assignment');
