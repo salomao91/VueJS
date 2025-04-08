0602c913725961c971e2efdc2c5993bf72c17f86
salomao91 <salomao1991@gmail.com>
2025-04-08 15:47:47 -0400

    monster-game - add style using compute and :style vue for update health bars

--- course-project-monster-game/app.js
+++ course-project-monster-game/app.js
@@ -1,23 +1,29 @@
 function getRandomValue(min, max) {
-    Math.floor(Math.random() * (max - min)) + min;
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 const app = Vue.createApp({
     data() {
         return {
             playerHealth: 100,
-            monsterhealth: 100
+            monsterHealth: 100
+        };
+    },
+    computed: {
+        monsterBarStyles() {
+            return { width: this.monsterHealth + '%' };
+        },
+       playerBarStyles() {
+            return { width: this.playerHealth + '%' };
         }
     },
     methods: {
         attackMonster() {
-            console.log('atackMonster');
             const attackValue = getRandomValue(5, 12);
-            this.monsterhealth -= attackValue;
+            this.monsterHealth -= attackValue;
             this.attackPlayer();
         },
         attackPlayer() {
-            console.log('atackPlayer');
             const attackValue = getRandomValue(8, 15);
             this.playerHealth -= attackValue;
         }
