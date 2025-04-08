d08a0963d38826dc16273c2f997b4b10ab40a19e
salomao91 <salomao1991@gmail.com>
2025-04-08 14:52:37 -0400

    course-project-monster-game - add data and  methods attackmonster and attackPlayer logic

--- course-project-monster-game/app.js
+++ course-project-monster-game/app.js
@@ -1,10 +1,25 @@
+function getRandomValue(min, max) {
+    Math.floor(Math.random() * (max - min)) + min;
+}
+
 const app = Vue.createApp({
     data() {
         return {
-
+            playerHealth: 100,
+            monsterhealth: 100
         }
     },
     methods: {
-
+        attackMonster() {
+            console.log('atackMonster');
+            const attackValue = getRandomValue(5, 12);
+            this.monsterhealth -= attackValue;
+            this.attackPlayer();
+        },
+        attackPlayer() {
+            console.log('atackPlayer');
+            const attackValue = getRandomValue(8, 15);
+            this.playerHealth -= attackValue;
+        }
     }
 }).mount('#game');
