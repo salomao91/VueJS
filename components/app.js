8b1a5ae3e1c1f158d39fee82c5fc432dc94e8a25
salomao91 <salomao1991@gmail.com>
2025-04-11 09:43:33 -0400

    components - after creating a component

--- components/app.js
+++ components/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
   data() {
     return {
-      detailsAreVisible: false,
       friends: [
         {
           id: 'manuel',
@@ -16,6 +15,36 @@ const app = Vue.createApp({
           email: 'julie@localhost.com',
         },
       ],
+    };
+  }
+});
+
+//a vue component is essentially just another Vue app that belongs to an app.
+//components are reusable pieces of html with connected data and logic
+
+//html identifier which will be used later in the html file
+app.component('friend-contact', {
+  template: `
+          <li>
+          <h2>{{ friend.name }}</h2>
+          <button @click="toggleDetails()">
+            {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
+          </button>
+          <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+          </ul>
+        </li>
+        `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 'manuel',
+        name: 'Manuel Lorenz',
+        phone: '01234 5678 991',
+        email: 'manuel@localhost.com',
+      }
     };
   },
   methods: {
