f568f0c7a14d4cb57b89fed65ed37e0a6fb2cb08
salomao91 <salomao1991@gmail.com>
2025-04-16 15:37:19 -0400

    add App.vue with a template, its data and imported it in main.js

---
+++ vue-project-created-with-cli/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "John Smith",
+          phone: "123 456 5768",
+          email: "john@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Roberts",
+          phone: "321 123 4345",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
