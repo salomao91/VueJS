6c7cd6089ebf9616bed494d53e872ec2b543969a
salomao91 <salomao1991@gmail.com>
2025-04-23 09:38:36 -0400

    add a new form - NewFriend component - and placed its tag <NewFriend> in the parent  component to show up

--- vue-project-created-with-cli/src/App.vue
+++ vue-project-created-with-cli/src/App.vue
@@ -3,6 +3,9 @@
     <header>
       <h1>My Friends</h1>
     </header>
+
+    <new-friend></new-friend>
+    
     <ul>
       <friend-contact
         v-for="friend in friends"
@@ -81,7 +84,8 @@ header {
   list-style: none;
 }
 
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -114,4 +118,19 @@ header {
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
 }
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
+}
+
 </style>
---
+++ vue-project-created-with-cli/src/components/NewFriend.vue
@@ -0,0 +1,25 @@
+<template>
+  <form>
+    <div>
+      <label>Name</label>
+      <input type="text" />
+    </div>
+    <div>
+      <label>Phone</label>
+      <input type="tel" />
+    </div>
+    <div>
+      <label>Email</label>
+      <input type="email" />
+    </div>
+    <div>   
+      <button>Add Contact</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+
+};
+</script>
