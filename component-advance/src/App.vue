d769d0a7ff439fcff07fa0a1bc7cf73a58dd502f
salomao91 <salomao1991@gmail.com>
2025-04-30 15:51:01 -0400

    component-advance - add scoped slot in CourseGoals component to be able to access its array data from another component.

--- component-advance/src/App.vue
+++ component-advance/src/App.vue
@@ -7,6 +7,12 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals>
+      <template v-slot:default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps.anotherProp }}</p>
+      </template>
+    </course-goals>
   </div>
 </template>
 
@@ -14,17 +20,19 @@
 import TheHeader from "./components/TheHeader.vue";
 import BadgeList from "./components/BadgeList.vue";
 import UserInfo from "./components/UserInfo.vue";
+import CourseGoals from "./components/CourseGoals.vue";
 
 export default {
   components: {
     "the-header": TheHeader,
     "badge-list": BadgeList,
     "user-info": UserInfo,
+    "course-goals": CourseGoals,
   },
   data() {
     return {
       activeUser: {
-        name: 'Maximilian Schwarzmüller',
+        name: 'Salomao Ferreira',
         description: 'Site owner and admin',
         role: 'admin',
       },
