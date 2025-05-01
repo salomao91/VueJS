0b39eff4e5d38606d51e05d4fd79531015e5aa5f
salomao91 <salomao1991@gmail.com>
2025-05-01 11:34:36 -0400

    component-advance - add teleport (built-in vue feature for semantic) render a block of code to an HTML markup we want.

--- component-advance/src/components/ManageGoals.vue
+++ component-advance/src/components/ManageGoals.vue
@@ -3,11 +3,15 @@
     <h2>Manage Goals</h2>
     <input type="text" ref="goal" />
     <button @click="setGoal">Set Goal</button>
-    <error-alert v-if="inputIsInvalid">
-      <h2>Input is invalid!</h2>
-      <p>Please enter at least a few caracters...</p>
-      <button @click="confirmError">Okay</button>
-    </error-alert>
+
+    <!-- (optional) teleport (built-in vue feature for semantic) render this block of code to an HTML markup we want -->
+    <teleport to="body">
+      <error-alert v-if="inputIsInvalid">
+        <h2>Input is invalid!</h2>
+        <p>Please enter at least a few caracters...</p>
+        <button @click="confirmError">Okay</button>
+      </error-alert>
+    </teleport>
   </div>
 </template>
 
