2ef4983eef31296fb44ab971ce2e00762a77c451
salomao91 <salomao1991@gmail.com>
2025-05-01 09:40:50 -0400

    component-advance - add dynamic components concept.

--- component-advance/src/App.vue
+++ component-advance/src/App.vue
@@ -1,24 +1,37 @@
 <template>
   <div>
     <the-header></the-header>
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <component :is="selectedComponent"></component>
   </div>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
 
 export default {
   components: {
     "the-header": TheHeader,
+    "active-goals": ActiveGoals,
+    "manage-goals": ManageGoals,
   },
   data() {
     return {
+      selectedComponent: "active-goals",
       activeUser: {
         name: 'Salomao Ferreira',
         description: 'Site owner and admin',
         role: 'admin',
       },
     };
+  },
+  methods: {
+    setSelectedComponent(component) {
+      this.selectedComponent = component;
+    },
   },
 };
 </script>
