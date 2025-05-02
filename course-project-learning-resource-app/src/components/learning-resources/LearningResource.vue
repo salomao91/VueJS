97f9e35ef60a9ec6a286884b0ba24303f51a16d1
salomao91 <salomao1991@gmail.com>
2025-05-02 16:50:13 -0400

    learning-resource-app - add BaseButton component, its style scoped (local), import it globally in main.js and make it reusable using slot and props concepts.

--- course-project-learning-resource-app/src/components/learning-resources/LearningResource.vue
+++ course-project-learning-resource-app/src/components/learning-resources/LearningResource.vue
@@ -3,7 +3,7 @@
     <base-card>
       <header>
         <h3>{{ title }}</h3>
-        <button>Delete</button>
+        <base-button mode="flat">Delete</base-button>
       </header>
       <p>{{ description }}</p>
       <nav>
@@ -14,6 +14,7 @@
 </template>
 
 <script>
+
 export default {
   props: ['title', 'description', 'link'],
 };
