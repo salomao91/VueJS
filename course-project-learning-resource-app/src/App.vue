b4cd0a89941abe6a4a5d76b5c45e6428f10c98b5
salomao91 <salomao1991@gmail.com>
2025-05-02 11:11:29 -0400

    learning-resource-app - add LearningResource.vue component, import it in App.vue component and passing data to it using v-for and props.

---
+++ course-project-learning-resource-app/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="resource in storedResources" :key="resource.id"
+      :title="resource.title"
+      :description="resource.description"
+      :link="resource.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
+export default {
+  components: {
+    LearningResource,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vue.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
