356f89f321deac7345491a09d6df3688c5c939c7
salomao91 <salomao1991@gmail.com>
2025-05-05 11:27:16 -0400

    learning-resource-app - add TheResources import in the App.vue and add its tag to be rendered in html.

--- course-project-learning-resource-app/src/App.vue
+++ course-project-learning-resource-app/src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <the-header title="RememberMe"></the-header>
+  <the-resources></the-resources>
 </template>
 
 <script>
 import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/learning-resources/TheResources.vue';
 
 export default {
   components: {
     TheHeader,
+    TheResources
   },
 };
 </script>
