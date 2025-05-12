f6680a82de17d65a40759a7e282f46ebfdd30d84
salomao91 <salomao1991@gmail.com>
2025-05-12 16:56:20 -0400

    sending-http-request - sending a POST request to firebase store data.

--- sending-http-request/src/App.vue
+++ sending-http-request/src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <learning-survey @survey-submit="storeSurvey"></learning-survey>
-  <user-experiences :results="savedSurveyResults"></user-experiences>
+  <learning-survey></learning-survey>
+  <user-experiences></user-experiences>
 </template>
 
 <script>
@@ -13,20 +13,8 @@ export default {
     UserExperiences,
   },
   data() {
-    return {
-      savedSurveyResults: [],
-    };
   },
   methods: {
-    storeSurvey(surveyData) {
-      const surveyResult = {
-        name: surveyData.userName,
-        rating: surveyData.rating,
-        id: new Date().toISOString(),
-      };
-      this.savedSurveyResults.push(surveyResult);
-      console.log(surveyResult);
-    },
   },
 };
 </script>
