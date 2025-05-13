1a9fcf80738e974fbb75ce3495288d077227fd7d
salomao91 <salomao1991@gmail.com>
2025-05-13 14:59:53 -0400

    sending-http-request - add a message to the user when there is no data.

--- sending-http-request/src/components/survey/UserExperiences.vue
+++ sending-http-request/src/components/survey/UserExperiences.vue
@@ -8,7 +8,8 @@
         >
       </div>
       <p v-if="isLoading">Loading...</p>
-      <ul v-else>
+      <p v-else-if="!isLoading && (!results || results.length === 0)">No stored experiences found. Add some servey results first.</p>
+      <ul v-else-if="!isLoading && results && results.length > 0">
         <survey-result
           v-for="result in results"
           :key="result.id"
