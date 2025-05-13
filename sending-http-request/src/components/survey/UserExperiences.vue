0dcfdb127533525f1c544296cab207f0b2f026a2
salomao91 <salomao1991@gmail.com>
2025-05-13 11:53:04 -0400

    sending-http-request - sending a GET request to firebase store data, parse JSON format to ARRAY and show it in the browser.

--- sending-http-request/src/components/survey/UserExperiences.vue
+++ sending-http-request/src/components/survey/UserExperiences.vue
@@ -3,7 +3,9 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="getExperiences"
+          >Load Submitted Experiences</base-button
+        >
       </div>
       <ul>
         <survey-result
@@ -21,9 +23,39 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+    };
+  },
+  methods: {
+    getExperiences() {
+      fetch(
+        'https://vue-http-request-9e50e-default-rtdb.firebaseio.com/surveys.json'
+      )
+        // remplaced "function()" to "=>" method to be able to refer to this.results.
+        // .then(function (response) {
+        .then( (response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        // .then(function (data) {
+        .then( (data) => {
+          const dbResults = [];
+          for (const id in data) {
+            dbResults.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+          this.results = dbResults;
+        });
+    },
   },
 };
 </script>
