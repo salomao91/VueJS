de513e13c3121a1e063946d9e38d369fd6ef631e
salomao91 <salomao1991@gmail.com>
2025-05-20 13:54:19 -0400

    routing-vue - add a redirect route for a  NotFound page/component (catch all routes).

--- vue-routing/src/main.js
+++ vue-routing/src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue';
 import TeamList from './components/teams/TeamsList.vue';
 import UserList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -15,6 +16,7 @@ const router = createRouter({
         { path: '/teams/:teamId', component:TeamMembers, props: true },
         // props: true => it tells the vue router that the dynamic parameters should be passed into this component as props rather than just on the $route property.
         // teamId is passed as a prop into this component when it's loaded.
+        { path: '/:notFound(.*)', component: NotFound }
     ],
     // we can override the router-link-active default class name
     linkActiveClass: 'active'
