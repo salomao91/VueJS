1d8df1dd3b41c0f8e2d8fe2354a821c6f14ab16d
salomao91 <salomao1991@gmail.com>
2025-05-21 17:51:26 -0400

    routing-vue - add navigation guard beforeRouteLeave to be triggered before user leaves the page based on a condition.

--- vue-routing/src/components/users/UsersList.vue
+++ vue-routing/src/components/users/UsersList.vue
@@ -1,5 +1,6 @@
 <template>
 <button @click="confirmInput">Confirm</button>
+<button @click="saveChanges">Save Changes</button>
   <ul>
     <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
   </ul>
@@ -13,17 +14,41 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  data() {
+    return {
+      changesSaved: false
+    };
+  },
   methods: {
     confirmInput() {
       this.$router.push('/teams');
       // this.$router.back();
       // this.$router.forward();
+    },
+    saveChanges() {
+      this.changesSaved = true;
     }
   },
   beforeRouteEnter(to, from, next) {
     console.log('UsersList component beforeEach');
     console.log(to, from);
     next();
+  },
+
+  // it's called when user wants to leave a page (before the beforeEach and beforeEnter guard methods).
+  // it's useful for when a user send a form data and it was not successfully saved, the page can warn him about it.
+  beforeRouteLeave(to, from, next) {
+    console.log('UsersList component beforeRouteLeave');
+    console.log(to, from);
+    if (this.changesSaved) {
+      next();
+    } else {
+      const userWantsToLeave = confirm('Are you sure ? You got unsaved changes!');
+      next(userWantsToLeave);
+    }
+  },
+  unmounted() {
+    console.log('UsersList unmounted')
   }
 };
 </script>
