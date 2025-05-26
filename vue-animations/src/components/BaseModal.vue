78f8e7f7919b51435e45a482e7a037fac4550b06
salomao91 <salomao1991@gmail.com>
2025-05-26 11:50:52 -0400

    vue-animations - add animtation for opening and closing a modal with transition component.

--- vue-animations/src/components/BaseModal.vue
+++ vue-animations/src/components/BaseModal.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="backdrop" @click="$emit('close')"></div>
-  <dialog open>
-    <slot></slot>
-  </dialog>
+  <div v-if="open" class="backdrop" @click="$emit('close')"></div>
+  <transition name="modal">
+    <dialog open v-if="open">
+      <slot></slot>
+    </dialog>
+  </transition>
 </template>
 
 <script>
 export default {
+  props: ['open'],
   emits: ['close'],
 };
 </script>
@@ -34,19 +37,26 @@ dialog {
   background-color: white;
   z-index: 100;
   border: none;
-  animation: modal 0.3s ease-out forwards;
-
+  /* animation: modal 0.3s ease-out forwards; */
 }
+
+ .modal-enter-active {
+  animation: modal 1s ease-out;
+ }
+
+ .modal-leave-active {
+  animation: modal 1s ease-in reverse;
+ }
 
 @keyframes modal {
   0% {
     opacity: 0;
-    transform: translateX(-50px) scale(0.9);
+    transform: translateY(-50px) scale(0.9);
   }
 
   100% {
     opacity: 1;
-    transform: translateX(0) scale(1);
+    transform: translateY(0) scale(1);
   }  
 }
 </style>
