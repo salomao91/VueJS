ccbb02de95027e065e3092159656b955bf8f0515
salomao91 <salomao1991@gmail.com>
2025-06-06 15:38:43 -0400

    vuex - add mapGetters.

--- vuex/src/components/TheCounter.vue
+++ vuex/src/components/TheCounter.vue
@@ -1,13 +1,20 @@
 <template>
-     <h3>{{ counter }}</h3>
+     <h3>{{ finalCounter }}</h3>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
-  computed: {
-    counter() {
-      return this.$store.getters.finalCounter;
-    }
-  },    
-}
+   computed: {
+    // counter() {
+    //     return this.$store.getters.finalCounter;
+    //   }
+    // },
+
+    // it gives an object that merge into the computed properties object
+    // getterNames as array parameter
+    ...mapGetters(['finalCounter'])
+    },
+};
 </script>
