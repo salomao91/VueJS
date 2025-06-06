b0887327f860f05274bcc651f0c806b5aba02cb9
salomao91 <salomao1991@gmail.com>
2025-06-06 15:46:55 -0400

    vuex - add mapActions

--- vuex/src/components/ChangeCounter.vue
+++ vuex/src/components/ChangeCounter.vue
@@ -1,14 +1,27 @@
 <template>
-    <button @click="addOne">Add 2</button>
+    <button @click="increment">Add 2</button>
+    <button @click="increase({value: 11})">Add 11</button>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
     methods: {
-        addOne() {
-            // never add the state inside a component, instead we only commit it to centralize the logic in the store.
-            this.$store.dispatch('increment');
-        }
+        // addOne() {
+        //     // never add the state inside a component, instead we only commit it to centralize the logic in the store.
+        //     this.$store.dispatch('increment');
+        // }
+
+        // it gives an object that merge into the methods properties object
+        // actionNames as array parameter
+        ...mapActions(['increment', 'increase'])
+
+        // an altertive way by renaming its action names here
+        // ... mapActions({
+        //     inc: 'increment',
+        //     increase: 'increase'
+        // })
     }
 }
 </script>
