6c187ff97c3f6781070e09a3d0d6809c7ad640bc
salomao91 <salomao1991@gmail.com>
2026-09-02 16:15:50 -0400

    add route page components and filled routes.js with them.

--- course-project-find-a-coach-webapp/src/router.js
+++ course-project-find-a-coach-webapp/src/router.js
@@ -1,17 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/coaches/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import NotFound from './pages/NotFound.vue';
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', redirect: '/coaches' },
-        { path: '/coaches', component: null },
-        { path: '/coaches/:id', component: null, children: [
-            { path: 'contact', component: null },   // /coaches/c1/contact
+        { path: '/coaches', component: CoachesList },
+        { 
+            path: '/coaches/:id', 
+            component: CoachDetail, 
+            children: [
+                { path: 'contact', component: ContactCoach },   // /coaches/c1/contact
         ] },
-        { path: '/register', component: null },
-        { path: '/contact', component: null },
-        { path: '/requests', component: null },
-        { path: '/:notFound(.*)', component: null }  // catch all 404
+        { path: '/register', component: CoachRegistration },
+        { path: '/requests', component: RequestsReceived },
+        { path: '/:notFound(.*)', component: NotFound }  // catch all 404
     ]
 });
 
