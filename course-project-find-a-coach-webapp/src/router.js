3bb3b4845e689630eac7f86cb91e178c7c6622c4
salomao91 <salomao1991@gmail.com>
2026-09-02 15:47:33 -0400

    registering routes in router.js

---
+++ course-project-find-a-coach-webapp/src/router.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', redirect: '/coaches' },
+        { path: '/coaches', component: null },
+        { path: '/coaches/:id', component: null, children: [
+            { path: 'contact', component: null },   // /coaches/c1/contact
+        ] },
+        { path: '/register', component: null },
+        { path: '/contact', component: null },
+        { path: '/requests', component: null },
+        { path: '/:notFound(.*)', component: null }  // catch all 404
+    ]
+});
+
+export default router;
