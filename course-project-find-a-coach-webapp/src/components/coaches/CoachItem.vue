5d88977419f238f5e7221fd5eb97af73cec7876b
salomao91 <salomao1991@gmail.com>
2026-09-08 16:39:29 -0400

    add BaseButton component as a wrapper and use it in CoachesList and CoachItem pages

--- course-project-find-a-coach-webapp/src/components/coaches/CoachItem.vue
+++ course-project-find-a-coach-webapp/src/components/coaches/CoachItem.vue
@@ -6,14 +6,16 @@
       <span v-for="area in areas" :key="area">{{ area }} </span>
     </div>
     <div class="actions">
-      <router-link :to="coachContactLink">Contact</router-link>
-      <router-link :to="coachDetailsLink">View Details</router-link>
+      <base-button mode="outline" :isLink="true" :to="coachContactLink">Contact</base-button>
+      <base-button :isLink="true" :to="coachDetailsLink">View Details</base-button>
     </div>
   </li>
 </template>
 
 <script>
+import BaseButton from '../ui/BaseButton.vue';
 export default {
+  components: { BaseButton },
   props: ['id','firstName', 'lastName', 'rate', 'areas'],
   computed: {
     fullName() {
