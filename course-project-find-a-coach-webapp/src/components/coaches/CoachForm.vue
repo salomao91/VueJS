91499fceb4337765df56337374beb7088ba62991
salomao91 <salomao1991@gmail.com>
2026-09-11 12:56:05 -0400

    wrap up the CoachForm with the reusable Basecard component and apply style.

--- course-project-find-a-coach-webapp/src/components/coaches/CoachForm.vue
+++ course-project-find-a-coach-webapp/src/components/coaches/CoachForm.vue
@@ -1,12 +1,12 @@
 <template>
   <form @submit.prevent="submitForm">
     <div class="form-control">
-      <label for="firstname">Firstname</label>
-      <input type="text" id="firstname" v-mode.trim="firstName" />
+      <label for="firstName">Firstname</label>
+      <input type="text" id="firstName" v-model.trim="firstName" />
     </div>
     <div class="form-control">
-      <label for="lastname">Lastname</label>
-      <input type="text" id="lastname" v-model.trim="lastName" />
+      <label for="lastName">Lastname</label>
+      <input type="text" id="lastName" v-model.trim="lastName" />
     </div>
     <div class="form-control">
       <label for="description">description</label>
@@ -39,9 +39,9 @@
 export default {
   data() {
     return {
-      firstNam: '',
+      firstName: '',
       lastName: '',
-      descript: '',
+      description: '',
       rate: 0,
       areas: [],
     };
@@ -53,7 +53,7 @@ export default {
         lastName: this.lastName,
         description: this.description,
         rate: this.rate,
-        ares: this.areas,
+        areas: this.areas,
       };
 
       console.log(formData);
@@ -61,3 +61,60 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input[type='checkbox'] + label {
+  font-weight: normal;
+  display: inline;
+  margin: 0 0 0 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  border: 1px solid #ccc;
+  font: inherit;
+}
+
+input:focus,
+textarea:focus {
+  background-color: #f0e6fd;
+  outline: none;
+  border-color: #3d008d;
+}
+
+  input[type='checkbox'] {
+    display: inline;
+    width: auto;
+    border: none;
+  }
+
+  input[type='checkbox']:focus {
+    outline: #3d008d solid 1px;
+  }
+
+  h3 {
+    margin: 0.5rem 0;
+    font-size: 1rem;
+  }
+
+  .invalid label {
+    color: red;
+  }
+
+  .invalid input,
+  .invalid textarea {
+    border: 1px solid red;
+  }
+</style>
